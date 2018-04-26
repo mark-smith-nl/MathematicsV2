b4a2bef9d802ac7969f2b2e2eeb73cfb112a2211
Mark Smith (nl) <mark_smith_nl@yahoo.co.uk>
2018-04-26 18:50:07 +0200

    Test for number patterns

--- src/main/java/nl/smith/mathematics/patterns/ExpressionElement.java
+++ src/main/java/nl/smith/mathematics/patterns/ExpressionElement.java
@@ -4,57 +4,56 @@ public class ExpressionElement {
 
 	/**
 	 * <pre>
-	 *  ℕ*: 1, 2, 3, ... (positive integer)
+	 *  ℕ*: 1, 2, 3, ... (natural number - not zero)
 	 * <pre>
 	 */
-	public static final String POSITIVE_NATURAL_NUMBER = "[1-9]\\d*";
-
-	/** 
-	 * <pre>
-	 * ℕ0: 0, 1, 2, 3, ... (non negative integer)
-	 * </pre>
-	 */
-	public static final String NATURAL_NUMBER = "0|" + POSITIVE_NATURAL_NUMBER;
+	public static final String POSITIVE_INTEGER = "[1-9]\\d*";
 
 	/** 
 	 * <pre>
 	 * ℤ: ..., -3, -2, -1, 0, 1, 2, 3, ... (integer)
 	 * </pre>
 	 */
-	public static final String INTEGER = "[\\-]?" + POSITIVE_NATURAL_NUMBER + "|0";
+	public static final String INTEGER = "[\\-]?" + POSITIVE_INTEGER + "|0";
 
 	/**
 	 * <pre>
-	 * A fraction begins with a dot, followed by zero or more (decimal) digits and ends with a non zero digit.
-	 * Trailing zeros are <b>not</b> accepted.The pattern contains two groups:
+	 * A fraction begins with a dot, followed by either:
+	 * 
+	 *  - zero or more (decimal) digits ending with a non zero digit
+	 *    Trailing zeros are <b>not</b> accepted.
+	 *  - zero or more digits, a list off one or more digits enclosed within pipes (repeating fraction).
+	 *    The repeating fraction should contain at least one non zero digit.
 	 * 
 	 * <b>First group</b>: decimal point
 	 * <b>Second group</b>: integer part with zero or more leading zeros (list of decimals)
 	 * </pre>
 	 */
 	public static final String FRACTION = "(\\.)(\\d*[1-9])";
-
 	/**
 	 * <pre>
-	 * Decimal number.
+	 * ℚ (Rational/Decimal number)
+	 * 
 	 * This number contains the following groups:
 	 * 
-	 * <b>First group</b>: decimal number (–∞, 1] ∪ [1, ∞)
-	 *     <b>Second group</b>: integer part of first group {n ∈ ℤ | n ∉ [0]}
+	 * <b>First group</b>: decimal number {n ∈ ℚ | (–∞, -1] ∪ [0, ∞)}
+	 *     <b>Second group</b>: integer part of first group {n ∈ ℤ}
 	 *     Third group: fractional part of first group
-	 *     Fourth group: decimal point
+	 *     Fourth group: decimal point of third group (fractional part)
 	 *     <b>Fifth group</b>: integer part  of third group with zero or more leading zeros (list of decimals)
-	 * <b>Sixth group</b>: decimal number (-1, 1)
-	 *     Seventh group: decimal point
-	 *     <b>Eight group</b>: integer part of third group with zero or more leading zeros (list of decimals)
+	 * <b>Sixth group</b>: decimal number {n ∈ ℚ | (-1, 0)}
+	 *     Seventh group: integer part of sixth group: always -0
+	 *     Eight group: fractional part of sixth group
+	 *     Ninth group: decimal point eight group (of fractional part)
+	 *     <b>Tenth group</b>: integer part  of eight group with zero or more leading zeros (list of decimals)
 	 * 
 	 * A string that matched this pattern has <b>either</b> a <b>not null</b> first group or sixth group.
 	 * If the first group is null then the second, third, fourth and fifth groups are also null.
-	 * If the sixth group is null then the seventh and eight groups are also null.
+	 * If the sixth group is null then the seventh, eight, ninth and tenth groups are also null.
 	 * 
 	 * </pre>
 	 */
-	public static final String DECIMAL_NUMBER = "((" + INTEGER + ")(" + FRACTION + ")?)" + "|(\\-0" + FRACTION + ")";
+	public static final String DECIMAL_NUMBER = "((" + INTEGER + ")(" + FRACTION + ")?)" + "|((\\-0)(" + FRACTION + "))";
 
 	/**
 	 * Example of numbers using scientific notation: −5.3×104 ===> -5.3E104
--- src/test/java/nl/smith/mathematics/patterns/ExpressionElementTest.java
+++ src/test/java/nl/smith/mathematics/patterns/ExpressionElementTest.java
@@ -3,16 +3,16 @@ package nl.smith.mathematics.patterns;
 import static nl.smith.mathematics.patterns.ExpressionElement.DECIMAL_NUMBER;
 import static nl.smith.mathematics.patterns.ExpressionElement.FRACTION;
 import static nl.smith.mathematics.patterns.ExpressionElement.INTEGER;
-import static nl.smith.mathematics.patterns.ExpressionElement.NATURAL_NUMBER;
-import static nl.smith.mathematics.patterns.ExpressionElement.POSITIVE_NATURAL_NUMBER;
+import static nl.smith.mathematics.patterns.ExpressionElement.POSITIVE_INTEGER;
 import static nl.smith.mathematics.patterns.ExpressionElement.SCIENTIFIC_NUMBER;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -23,24 +23,13 @@ import org.junit.Test;
 public class ExpressionElementTest {
 
 	@Test
-	public void positiveNaturalNumber() {
+	public void positiveInteger() {
 		for (int i = -100; i <= 0; i++) {
-			assertFalse(String.format("Value '%d' should not be accepted an positiveNaturalNumber.", i), String.valueOf(i).matches(POSITIVE_NATURAL_NUMBER));
+			assertFalse(String.format("Value '%d' should not be accepted an positive natural number.", i), String.valueOf(i).matches(POSITIVE_INTEGER));
 		}
 
 		for (int i = 1; i <= 100; i++) {
-			assertTrue(String.format("Value '%d' should be accepted an positiveNaturalNumber.", i), String.valueOf(i).matches(POSITIVE_NATURAL_NUMBER));
-		}
-	}
-
-	@Test
-	public void naturalNumber() {
-		Arrays.asList("a", " 1", "+1", "-1", "-11", "+11", "2.5", "-2.5", "2.5E2").forEach(value -> {
-			assertFalse(String.format("Value '%s' should not be accepted as naturalNumber.", value), value.matches(NATURAL_NUMBER));
-		});
-
-		for (int i = 0; i <= 100; i++) {
-			assertTrue(String.format("Value '%d' should be accepted as a naturalNumber.", i), String.valueOf(i).matches(NATURAL_NUMBER));
+			assertTrue(String.format("Value '%d' should be accepted an positive natural number.", i), String.valueOf(i).matches(POSITIVE_INTEGER));
 		}
 	}
 
@@ -70,54 +59,93 @@ public class ExpressionElementTest {
 		});
 	}
 
+	/*
+	 * @Test public void fraction2() { Pattern pattern = Pattern.compile(FRACTION); Arrays.asList(".|1|R", ".0|1|R", ".0234|12|R").forEach(value -> { Matcher matcher =
+	 * pattern.matcher(value); assertTrue(String.format("Value '%s' should be accepted as a fraction.", value), matcher.matches()); assertThat(matcher.groupCount(), is(2));
+	 * assertThat(matcher.group(1), is(".")); });
+	 * 
+	 * Arrays.asList(".||R", ".1|0|R", ".1|00|R").forEach(value -> { assertFalse(String.format("Value '%s' should not be accepted as a fraction.", value), value.matches(FRACTION));
+	 * });
+	 * 
+	 * }
+	 */
+
 	@Test
 	public void decimalNumber() {
-		Arrays.asList("2", "2.01", "-2", "-2.01", "0", "0.33", "-0.33", "0.2", "-0.2").forEach(value -> {
-			assertTrue(String.format("Value '%s' should be accepted as a decimal.", value), value.matches(DECIMAL_NUMBER));
+		List<Map<String, Object>> numberMapElements = new ArrayList<>();
+		Map<String, Object> valueMap = new HashMap<>();
+		valueMap.put("value", "2");
+		valueMap.put("expectedGroupValues", Arrays.asList("2", "2", "", "", "", null, null, null, null, null));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "2.01");
+		valueMap.put("expectedGroupValues", Arrays.asList("2.01", "2", ".01", ".", "01", null, null, null, null, null));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "-2");
+		valueMap.put("expectedGroupValues", Arrays.asList("-2", "-2", "", "", "", null, null, null, null, null));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "-2.01");
+		valueMap.put("expectedGroupValues", Arrays.asList("-2.01", "-2", ".01", ".", "01", null, null, null, null, null));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "0");
+		valueMap.put("expectedGroupValues", Arrays.asList(null, null, null, null, null, "0", "", ""));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "0.33");
+		valueMap.put("expectedGroupValues", Arrays.asList("0.33", "0", ".330", ".", "33", null, null, null, null, null));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "0.033");
+		valueMap.put("expectedGroupValues", Arrays.asList("0.033", "0", ".033", ".", "033", null, null, null, null, null));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "-0.33");
+		valueMap.put("expectedGroupValues", Arrays.asList(null, null, null, null, null, "-0.33", "-0", ".33", ".", "33"));
+		numberMapElements.add(valueMap);
+
+		valueMap.put("value", "-0.033");
+		valueMap.put("expectedGroupValues", Arrays.asList(null, null, null, null, null, "-0.033", "-0", ".033", ".", "033"));
+		numberMapElements.add(valueMap);
+
+		Pattern pattern = Pattern.compile(DECIMAL_NUMBER);
+
+		numberMapElements.forEach((map) -> {
+			String value = (String) map.get("value");
+			Matcher matcher = pattern.matcher(value);
+			List<String> expectedGroupValues = (List<String>) map.get("expectedGroupValues");
+
+			assertThat(matcher.matches(), is(true));
+			assertThat(matcher.groupCount(), is(10));
+
+			for (int i = 1; i <= matcher.groupCount(); i++) {
+				assertThat(String.format("Value '%s' should be accepted as a decimal number.", value), matcher.group(i), is(expectedGroupValues.get(i - 1)));
+			}
 		});
 
 		Arrays.asList("-0", "+0", "0.0", "+0.0", "-0.0", "2E4", "-2E4", "2.1E4", "-2.1E4").forEach(value -> {
-			assertFalse(String.format("Value '%s' should not be accepted as a decimal.", value), value.matches(DECIMAL_NUMBER));
+			assertFalse(String.format("Value '%s' should not be accepted as a decimal number.", value), value.matches(DECIMAL_NUMBER));
 		});
 
 	}
 
 	@Test
 	public void scientificNumber() {
-		Arrays.asList("2E3", "2.01E3", "-2E3", "-2.01E3", "0E3", "0.33E3", "-0.33E3", "0.2E3", "-0.2E3").forEach(value -> {
-			assertTrue(String.format("Value '%s' should be accepted as a decimal.", value), value.matches(SCIENTIFIC_NUMBER));
+		Arrays.asList("2E3", "2.01E3", "-2E3", "-2.01E3", "2E-3", "2.01E-3", "-2E-3", "-2.01E-3").forEach(value -> {
+			assertTrue(String.format("Value '%s' should be accepted as a scientific number.", value), value.matches(SCIENTIFIC_NUMBER));
 		});
 
-		/*
-		 * Arrays.asList("-0", "+0", "0.0", "+0.0", "-0.0", "2E4", "-2E4", "2.1E4", "-2.1E4").forEach(value -> {
-		 * assertFalse(String.format("Value '%s' should not be accepted as a decimal.", value), value.matches(DECIMAL_NUMBER)); });
-		 */
+		Arrays.asList("-0.33E3", "0.2E3", "-0.2E3", "0.33E3", "0E3", "-0", "+0", "0.0", "+0.0", "-0.0").forEach(value -> {
+			assertFalse(String.format("Value '%s' should not be accepted as a cientific number.", value), value.matches(SCIENTIFIC_NUMBER));
+		});
 
 	}
 
 	public static void main(String[] args) {
-		System.out.println(SCIENTIFIC_NUMBER);
-		System.out.println(DECIMAL_NUMBER);
-		// (([\-]?[1-9]\d*|0)((\.)(\d*[1-9]))?)|(\-0(\.)(\d*[1-9]))
-		// ab Bcd De ECB A f g Gh HF
-		Pattern pattern = Pattern.compile(DECIMAL_NUMBER);
-
-		Map<String, List<String>> valueMap = new LinkedHashMap<>();
-		valueMap.put("-2.05", Arrays.asList("", "", "", "", "", "", "", ""));
-		valueMap.forEach((key, value) -> {
-			Matcher matcher = pattern.matcher(key);
-			System.out.println(matcher.matches());
-			System.out.println(matcher.groupCount());
-			for (int i = 1; i <= matcher.groupCount(); i++) {
-				System.out.println(matcher.group(i));
-			}
-		});
-
-		/*
-		 * BigInteger numerator = new BigInteger("05"); BigInteger denominator = BigInteger.TEN.pow("05".length());
-		 * 
-		 * System.out.println(numerator); System.out.println(denominator);
-		 */
+		System.out.println(FRACTION);
+		// (\.)( (\d*[1-9])|(\d*\|\d*[1-9]\d*\|R) )
+		//
 	}
-
 }
